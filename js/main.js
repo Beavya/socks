884566e808586c90525a787175b69728c0987c80
Beavya <AnnBezymya@gmail.com>
2026-03-02 19:15:50 +0700

    Display reviews list

--- js/main.js
+++ js/main.js
@@ -85,6 +85,17 @@ Vue.component('product', {
                 <button @click="removeFromCart">Remove from cart</button>
                 <a :href="link">More products like this</a>
             </div>
+            <div>
+                <h2>Reviews</h2>
+                <p v-if="!reviews.length">There are no reviews yet.</p>
+                <ul>
+                    <li v-for="review in reviews">
+                        <p>{{ review.name }}</p>
+                        <p>Rating: {{ review.rating }}</p>
+                        <p>{{ review.review }}</p>
+                    </li>
+                </ul>
+            </div>
             <product-review @review-submitted="addReview"></product-review>
         </div>
     `,
