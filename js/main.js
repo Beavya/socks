3a57cdfe887945beabb38b9a7c60dc44d0b6da04
Beavya <AnnBezymya@gmail.com>
2026-03-02 16:06:16 +0700

    Add variants

--- js/main.js
+++ js/main.js
@@ -8,6 +8,16 @@ let app = new Vue({
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inStock: true,
         onSale: true,
-        details: ['80% cotton', '20% polyester', 'Gender-neutral']
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ]
     }
 })
