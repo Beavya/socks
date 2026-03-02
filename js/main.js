f36f78607c3ab371b406e13a54cd18c932263ed6
Beavya <AnnBezymya@gmail.com>
2026-03-02 16:17:14 +0700

    Add mouseover event to change product image

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ let app = new Vue({
     data: {
         product: "Socks",
         description: "A pair of warm, fuzzy socks",
-        image: "/assets/vmSocks-green-onWhite.jpg",
+        image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inStock: true,
@@ -12,11 +12,13 @@ let app = new Vue({
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'green'
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg"
             },
             {
                 variantId: 2235,
-                variantColor: 'blue'
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg"
             }
         ],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
@@ -25,6 +27,9 @@ let app = new Vue({
     methods: {
         addToCart() {
             this.cart += 1
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
         }
     }
 })
