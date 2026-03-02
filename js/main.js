7d0d056e8794a3255bc989effe2b19b888cdd0f4
Beavya <AnnBezymya@gmail.com>
2026-03-02 16:13:45 +0700

    Move cart logic to addToCart method

--- js/main.js
+++ js/main.js
@@ -21,5 +21,10 @@ let app = new Vue({
         ],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         cart: 0
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        }
     }
 })
